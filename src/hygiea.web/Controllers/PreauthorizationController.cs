c13fa51b68310bc4d5b18d0ba5dacf8a069d1c0f
Luc <luc9volts@pop-os.localdomain>
2021-08-30 22:50:56 -0300

    First

---
+++ src/hygiea.web/Controllers/PreauthorizationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace hygiea.web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PreauthorizationController : ControllerBase
+    {
+        private readonly ILogger<PreauthorizationController> _logger;
+
+        public PreauthorizationController(ILogger<PreauthorizationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult PostAuthorization()
+        {
+            return BadRequest();
+        }
+    }
+}
